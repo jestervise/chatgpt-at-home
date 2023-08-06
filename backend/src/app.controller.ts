3806da2dcffce80612a66f60bfda4b5384fff430
mingjunchang <changmingjun98@gmail.com>
2023-08-06 17:51:10 +0800

    init backend

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('video')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  findAll(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  createVideo(): string {
+    return this.appService.getHello();
+  }
+
+  @Delete()
+  deleteVideo(): string {
+    return this.appService.getHello();
+  }
+
+  @Patch()
+  updateVideo(): string {
     return this.appService.getHello();
   }
 }
